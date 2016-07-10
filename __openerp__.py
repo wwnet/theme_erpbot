99e27aaf529f3fc139160d399f5ffc51c80ac2ba
Alexis Yushin <alexis@ww.net>
2016-07-10 20:25:16 +0300

    The ERPBot theme skeleton.

---
+++ __openerp__.py
@@ -0,0 +1,25 @@
+{
+    # Theme information
+    'name': "theme_erpbot",
+    'description': """
+    The official ERPBot theme.
+    """,
+    'category': 'Theme',
+    'version': '1.0',
+    'depends': ['website'],
+
+    # templates
+    'data': [
+        'views/options.xml',
+        'views/snippets.xml',
+    ],
+
+    # demo pages
+    'demo': [
+        'demo/pages.xml',
+    ],
+
+    # Your information
+    'author': "ERPBot",
+    'website': "http://erpbot.com",
+}
